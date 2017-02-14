ec733c463da442ef3a8273a9e32a04f1576741bf
Ahmad Bastiar <nsjm.cs@gmail.com>
2017-02-14 14:46:31 +0700

    merapikan tampilan

--- theme/app/index.php
+++ theme/app/index.php
@@ -75,59 +75,82 @@
 				    </div>
 				  </div>
     		 </div>
-    		  <ul class="collapsible" data-collapsible="accordion">
-    		  	  <nav>
-				    <div class="nav-wrapper">
-				      <div class="col s12">
-				        <a href="#!" class="breadcrumb">HOME</a>
-				        <a href="#!" class="breadcrumb">JAVASCRIPT</a>
-				        <a href="#!" class="breadcrumb">DATEPICKER</a>
-				      </div>
-				    </div>
-				  </nav>
-			    <li>
-			      <div class="collapsible-header"><i class="material-icons">report_problem</i>DISABLE DATE YESTERDAY</div>
-			      <div class="collapsible-body">
-			      	  <div class="row">
-					    <form class="col s12">
-					      <div class="row">
-					        <div class="input-field col s6">
-					          <input type="date" class="datepicker">
-					        </div>
+    		 <div class="row">
+    		 	<div class="col s12">
+				    		 		
+				  <ul class="collapsible popout" data-collapsible="accordion">
+				    <li>
+				      <div class="collapsible-header"><i class="material-icons">filter_drama</i>JAVASCRIPT</div>
+				      <div class="collapsible-body">
+				      	<center><span>SHOWING CALCULATE DATE & PICKERDATE SAMPLE</span></center>
+				      	 <ul class="collapsible" data-collapsible="accordion">
+		    		  	  <nav>
+						    <div class="nav-wrapper">
+						      <div class="col s12">
+						        <a href="#!" class="breadcrumb">HOME</a>
+						        <a href="#!" class="breadcrumb">JAVASCRIPT</a>
+						        <a href="#!" class="breadcrumb">PIKCERDATE</a>
+						      </div>
+						    </div>
+						  </nav>
+					    <li>
+					      <div class="collapsible-header"><i class="material-icons">report_problem</i>DISABLE DATE YESTERDAY</div>
+					      <div class="collapsible-body">
+					      	  <div class="row">
+							    <form class="col s12">
+							      <div class="row">
+							        <div class="input-field col s6">
+							          <input type="date" class="datepicker">
+							        </div>
+							      </div>
+							    </form>
+							  </div>
+							  <div class="well">
+							  	http://amsul.ca/pickadate.js/date/
+							  </div>
 					      </div>
-					    </form>
-					  </div>
-					  <div class="well">
-					  	http://amsul.ca/pickadate.js/date/
-					  </div>
-			      </div>
-			    </li>
-			    <li>
-			      <div class="collapsible-header"><i class="material-icons">av_timer</i>GET VALUE 2 DATEPICER</div>
-			      <div class="collapsible-body">
-			      	<form>
-					    START DATE<br>
-					    <input type="date" id="start_date" name="firstname"  >
-					<br>
-					    END DATE<br>
-					    <input type="date" id="end_date" name="lastname">
-					      <button class="btn waves-effect waves-light" id='get_days'  type="button" name="action">Submit
-						    <i class="material-icons right">send</i>
-						  </button>
-					</form>
-				    <div class="row">
-				      <div class="col s12 m2">
-				        <div class="card-panel teal">
-				          <span class="white-text">
-				          <div class='days'>0
-							<div>
-					      	</div>	
-				          </span>
-				        </div>
+					    </li>
+					    <li>
+					      <div class="collapsible-header"><i class="material-icons">av_timer</i>GET VALUE 2 DATEPICER</div>
+					      <div class="collapsible-body">
+					      	<form>
+							    START DATE<br>
+							    <input type="date" id="start_date" name="firstname"  >
+							<br>
+							    END DATE<br>
+							    <input type="date" id="end_date" name="lastname">
+							      <button class="btn waves-effect waves-light" id='get_days'  type="button" name="action">Submit
+								    <i class="material-icons right">send</i>
+								  </button>
+							</form>
+						    <div class="row">
+						      <div class="col s12 m2">
+						        <div class="card-panel teal">
+						          <span class="white-text">
+						          <div class='days'>0
+									<div>
+							      	</div>	
+						          </span>
+						        </div>
+						      </div>
+						    </div>
+					    </li>
+					  </ul>
 				      </div>
-				    </div>
-			    </li>
-			  </ul>
+				    </li>
+				    <li>
+				      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+				      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+				    </li>
+				    <li>
+				      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+				      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+				    </li>
+				  </ul>
+				        
+    		 	</div>
+    		 </div>
+    		 
     	</div>
     	<footer class="page-footer">
           <div class="container">
@@ -189,7 +212,9 @@ $('#get_days').click(function(){
 </script>
 
 <script type="text/javascript">
+	// for sidnav bar
   $(".button-collapse").sideNav();
+  		// pickerdate & validate date
 	  var yesterday = new Date((new Date()).valueOf()-1000*60*60*24);
 	  $('.datepicker').pickadate({
 	 	   disable: [
@@ -208,4 +233,9 @@ $('#get_days').click(function(){
 	  ]
 	}
 	  	);
+	  // for collapsible
+	  $(document).ready(function(){
+	    $('.collapsible').collapsible();
+	  });
+	       
   </script>
