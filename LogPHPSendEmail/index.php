8b827f5b29b0a8145e133e1721d9ab548bf86a16
Ahmad Bastiar <nsjm.cs@gmail.com>
2017-03-02 11:09:59 +0700

    update remove folder

--- LogPHPSendEmail/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "nsjm.cs@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: ahmad.bastian8@gmail.com";
-
-mail($to,$subject,$txt,$headers);
-?>
